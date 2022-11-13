013369fde11a618a457bc6f4bf3fdfb3885abfd9
Tjex07 <tjex07@hotmail.com>
2022-11-13 16:44:37 -0700

    deleted:    .env.EXAMPLE 	modified:   models/Category.js 	modified:   models/Product.js commit

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
+// Defines Category with a primary key requiring input that auto increments
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+// Defines category_name to require input
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,44 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
+  //product has an ID, product_name, price, stock and category_id.
   {
-    // define columns
+    // id is a primary key accepting numberic data input that cannot be null.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product_name is a dataType that accepts strings and cannot be null
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // price accepts dataTypes that are decimal friendly and cannot be null
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+      // stock accepts dataTypes that are Integers and cannot be null
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+       // category_id accepts dataTypes that are Integers, cannot be null, and is a foreign key to category.
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    },
   },
   {
     sequelize,
